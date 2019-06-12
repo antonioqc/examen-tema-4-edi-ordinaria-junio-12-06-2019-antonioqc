f07c5d111a46b4e36d0c55aa1e381f4084f30402
Antonio Quesada <46671912+antonioqc@users.noreply.github.com>
2019-06-12 09:06:06 +0200

    Refactorizacion 1 Gato - Antonio Quesada Cuadrado

---
+++ Gato.java
@@ -0,0 +1,24 @@
+
+/**
+ * Clase gato
+ * @author antonio
+ *
+ */
+
+public class Gato {
+
+  private static final String MAULLA = "Miau";
+
+  /**
+   * Constructor
+   */
+  
+  protected Gato() {
+      
+}
+  
+void maullar() {
+  System.out.println(MAULLA);
+}
+
+}
